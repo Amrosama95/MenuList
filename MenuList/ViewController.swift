1dae9cc8c67859d46ff0d7b76d7b3624b375729e
AmrOsama <amrosama2@amrosamas-mac.local>
2019-02-09 21:15:18 +0200

    Initial Commit

---
+++ MenuList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuList
+//
+//  Created by AmrOsama on 2/9/19.
+//  Copyright © 2019 AmrOsama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
